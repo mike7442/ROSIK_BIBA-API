75e804678f829cbcefd6b0f5da68e01be0b0f4e2
mike7442 <mikhail.popov.alexandrovich@gmail.com>
2026-06-01 13:43:04 +0300

    Update demo_server.py

--- demo_TCP/demo_server.py
+++ demo_TCP/demo_server.py
@@ -1,22 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import socket
 
-# Задаем адрес сервера
-SERVER_ADDRESS = ('192.168.0.221', 31415)
+sock = socket.socket()
+sock.bind(('192.168.0.221', 27182))
+sock.listen(1)
+conn, addr = sock.accept()
 
-# Настраиваем сокет
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(SERVER_ADDRESS)
-server_socket.listen(10)
-print('server is running, please, press ctrl+c to stop')
+print( 'connected:', addr)
 
-# Слушаем запросы
-while True:
-    connection, address = server_socket.accept()
-    print("new connection from {address}".format(address=address))
 
-    data = connection.recv(1024)
-    print(str(data))
+data = conn.recv(1024)
+conn.send(data.upper())
 
-    connection.send(bytes('Hello from server!', encoding='UTF-8'))
-
-    connection.close()
+conn.close()
